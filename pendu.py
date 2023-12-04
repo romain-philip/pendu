640fd2f13650f93d02650a82686de62167beeac8
Romain Philip <146707204+romain-philip@users.noreply.github.com>
2023-12-04 10:49:45 +0100

    Update pendu.py

--- pendu.py
+++ pendu.py
@@ -4,16 +4,16 @@ def play_hangman():
    
     words = ["python", "programmation", "ordinateur", "algorithme", "variable", "fonction", "boucle", "condition", "syntaxe", "objet", "valouzz", "flatulence", "floop", "arnaquer", "frauder", "cagoule"]
 
-   
+    
     word = random.choice(words)
 
-   
+    
     guessed_letters = []
-    tries = 6
+    tries = 7
 
-   
+    
     while True:
-        
+       
         display_word = ""
         for letter in word:
             if letter in guessed_letters:
@@ -22,11 +22,20 @@ def play_hangman():
                 display_word += "_"
         print(display_word)
 
-       
-        if tries == 6:
+        
+        if tries == 7:
             print("  _________     ")
             print(" |/        |    ")
             print(" |               ")
+            print(" |               ")
+            print(" |               ")
+            print(" |               ")
+            print(" |               ")
+            print("_|___            ")
+        elif tries == 6:
+            print("  _________     ")
+            print(" |/        |    ")
+            print(" |        (_)   ")
             print(" |               ")
             print(" |               ")
             print(" |               ")
@@ -36,8 +45,8 @@ def play_hangman():
             print("  _________     ")
             print(" |/        |    ")
             print(" |        (_)   ")
-            print(" |               ")
-            print(" |               ")
+            print(" |         |     ")
+            print(" |         |     ")
             print(" |               ")
             print(" |               ")
             print("_|___            ")
@@ -47,7 +56,7 @@ def play_hangman():
             print(" |        (_)   ")
             print(" |         |     ")
             print(" |         |     ")
-            print(" |               ")
+            print(" |        /      ")
             print(" |               ")
             print("_|___            ")
         elif tries == 3:
@@ -56,19 +65,10 @@ def play_hangman():
             print(" |        (_)   ")
             print(" |         |     ")
             print(" |         |     ")
-            print(" |        /      ")
-            print(" |               ")
-            print("_|___            ")
-        elif tries == 2:
-            print("  _________     ")
-            print(" |/        |    ")
-            print(" |        (_)   ")
-            print(" |         |     ")
-            print(" |         |     ")
             print(" |        / \   ")
             print(" |               ")
             print("_|___            ")
-        elif tries == 1:
+        elif tries == 2:
             print("  _________     ")
             print(" |/        |    ")
             print(" |        (_)   ")
@@ -90,15 +90,15 @@ def play_hangman():
             print("Désolé, vous avez perdu. Le mot était :", word)
             break
 
-       
+        
         if "_" not in display_word:
             print("Bravo, vous avez gagné !")
             break
 
-       
+        
         letter = input("Entrez une lettre : ")
 
-       
+        
         if letter in guessed_letters:
             print("Vous avez déjà proposé cette lettre. Veuillez en choisir une autre.")
             continue
@@ -106,17 +106,19 @@ def play_hangman():
         
         guessed_letters.append(letter)
 
-       
+        
         if letter in word:
             print("La lettre est dans le mot !")
         else:
             print("La lettre n'est pas dans le mot.")
             tries -= 1
 
-       
+        
         if tries == 0:
             print("Désolé, vous avez perdu. Le mot était :", word)
             break
 
 
 play_hangman()
+
+
